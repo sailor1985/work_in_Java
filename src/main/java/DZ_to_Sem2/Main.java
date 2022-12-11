10624ee2bdb40299bf5bff73aa6f4e00174fa44d
Michael <cadetvire@rambler.ru>
2022-12-11 22:29:07 +0300

    Финиш задачи 2 (запись 100 раз слова TEST в файл txt) семинара 2

--- src/main/java/DZ_to_Sem2/Main.java
+++ src/main/java/DZ_to_Sem2/Main.java
@@ -2,19 +2,16 @@ package DZ_to_Sem2;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ex1("А роза упала на лапу Азора")); //Задача 1
-        //System.out.println(ex2("aaaabbbcdd")); //Задача 2
-
-//
-//        String str = "А роза упала на лапу Азора";
-//        String clean = str.replaceAll(" ", "").toLowerCase();
-//        System.out.println(clean);
-
+        System.out.println(checkingForPalindrome("А роза упала на лапу Азора")); //Задача 1
+        writeToFile(createStringCount(100, "TEST ")); //Задача 2
     }
-
-private static boolean ex1(String str) {
+//    Задача 1. Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).
+private static boolean checkingForPalindrome(String str) {
     String clean = str.replaceAll(" ", "").toLowerCase();
     int length = clean.length() - 1;
     int firstIndex = 0;
@@ -28,7 +25,28 @@ private static boolean ex1(String str) {
     }
 
 
+//    Задача 2. Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
+//    который запишет эту строку в простой текстовый файл, обработайте исключения.
+private static String createStringCount(int count, String str) {
+    StringBuilder new_str = new StringBuilder();
+    for (int i = 0; i < count; i++) {
+        new_str.append(str);
+    }
+    return new_str.toString();
+}
+
+private static void writeToFile(String str) {
+    try (FileWriter fw = new FileWriter("C:\\Users\\Клен-М\\Work in Git\\work_in_Java\\src\\main\\java\\DZ_to_Sem2\\file.txt", false)) {
+        fw.write(str);
+        fw.flush();
+    } catch (IOException ex) {
+        System.out.println(ex.getMessage());
+    }
 
 }
-//    1. Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).
+
+
+
+
+}
 
