90b059c06a94da6abd94311fdc30b5c668f91267
Michael <cadetvire@rambler.ru>
2022-12-17 16:29:52 +0300

    Финиш задачи 3 ДЗ 4

--- src/main/java/DZ_to_Sem4/MyQueue.java
+++ src/main/java/DZ_to_Sem4/MyQueue.java
@@ -1,28 +1,32 @@
 package DZ_to_Sem4;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class MyQueue {
-    private static LinkedList<String> queue =  new LinkedList<String>();
+    private static LinkedList<String> queue =  new LinkedList<>();
 
     public static void main(String[] args) {
-        add("hello"); //добавить в конец
-        System.out.println(top()); //посмотреть первое в очереди
-        System.out.println(remove()); //извлечь и вернуть первое в очереди
+        Collections.addAll(queue,"apple", "orange", "banana", "tomato", "strawberry", "melon");
+        System.out.println("Элементы стэка: " + queue);
+        //add("hello"); //добавить в конец
+        //System.out.println("Первое в очереди = " + top()); //посмотреть первое в очереди
+        //System.out.println("Первый извлеченный элемент из очереди = " + remove()); //извлечь и вернуть первое в очереди
     }
 
     private static void add(String hello) {
-
-                Queue<Integer> queue = new LinkedList<Integer>();
-
+        queue.add(0, hello);//добавили в начало очереди по индексу 0
+        System.out.println(queue);
     }
 
     private static String top() {
-        return "";
+        return queue.get(queue.size() -1);
     }
 
     private static String remove() {
-        return "";
+        String elem = queue.get(queue.size() -1);
+        queue.set(queue.size() -1, null);
+        return elem;
     }
 }
