a4071a4072a6049006db703e215db26131b053b7
Michael <cadetvire@rambler.ru>
2022-12-17 14:52:28 +0300

    Финиш задачи 1 ДЗ 4

---
+++ src/main/java/Sem4/MyQueue.java
@@ -0,0 +1,25 @@
+package Sem4;
+
+import java.util.LinkedList;
+
+public class MyQueue {
+    private static LinkedList<String> queue;
+
+    public static void main(String[] args) {
+        add("hello"); //добавить в конец
+        System.out.println(top()); //посмотреть первое в очереди
+        System.out.println(remove()); //извлечь и вернуть первое в очереди
+    }
+
+    private static void add(String hello) {
+
+    }
+
+    private static String top() {
+        return "";
+    }
+
+    private static String remove() {
+        return "";
+    }
+}
