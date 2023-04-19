d4421540207b7d64e2ef4c92983c89bc577ee4c3
Michael <cadetvire@rambler.ru>
2023-04-19 16:00:20 +0300

    Finish DZ 1

--- src/main/java/Exeptions/Sem1.java
+++ src/main/java/Exeptions/Sem1.java
@@ -1,6 +1,6 @@
 package Exeptions;
 
-public class Test1 {
+public class Sem1 {
     public static void main(String[] args) {
         //Задача 1:
         int [] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
