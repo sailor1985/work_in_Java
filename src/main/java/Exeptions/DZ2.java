713381d73f68cb78e48b330acb84906c29d7e382
Michael <cadetvire@rambler.ru>
2023-04-23 21:59:29 +0300

    task 3 DZ 2 done

--- src/main/java/Exeptions/DZ2.java
+++ src/main/java/Exeptions/DZ2.java
@@ -1,5 +1,6 @@
 package Exeptions;
 
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,14 +9,27 @@ public class DZ2 {
         //Задача 1:
         //System.out.println(floatValue());
         //Задача 2:
-        task2();
-
+        //task2();
+        //Задача 3:
+        try {
+            Integer a = 90;
+            Integer b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
+        }
     }
 
     /* Задача 1:
        Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение.
-       Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
-     */
+       Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.*/
     public static float floatValue() {
         while (true)
             try {
@@ -40,5 +54,13 @@ public class DZ2 {
             System.out.println("Поймано исключение: ArrayIndexOutOfBoundsException\n " + "Полное описание: " + e);
         }
     }
+    /* Задача 3: Если необходимо, исправьте данный код */
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+
+
+
 
 }
+
